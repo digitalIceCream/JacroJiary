afcba922204f50496d996f4c96cf31a1a4a7d70e
John-Philipp Vogt <john-philipp.vogt@mailbox.org>
2024-02-12 16:43:03 +0200

    First master branch

---
+++ jj.java
@@ -0,0 +1,35 @@
+/*
+Author:			John-Philipp Vogt
+Date:			2024-
+Synopsis:		
+Filename:		
+*/
+
+//import Ingredient;
+
+public class jj {
+
+	public static void main(String[] args) {
+
+		boolean isActive = false;
+
+			System.out.println("Hello user.");
+			System.out.println("Your current budget is:\n");
+			System.out.println("<daily macro budget placeholder>\n");
+
+		do {
+			System.out.println("Please choose:");
+			System.out.println("0 -- View current budget.");
+			System.out.println("1 -- Edit meal.");
+			System.out.println("2 -- Record a new meal.");
+			System.out.println("3 -- Exit program.");
+
+
+		} while (isActive);
+
+		System.out.println("End of program.");
+
+		}
+}
+
+
