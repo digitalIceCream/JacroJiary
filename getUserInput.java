6772578963fee2cc2ba09453a3c3ca5cf5033ee5
John-Philipp Vogt <john-philipp.vogt@mailbox.org>
2024-02-17 12:19:14 +0200

    branch setup

---
+++ getUserInput.java
@@ -0,0 +1,17 @@
+/*
+Author:			John-Philipp Vogt
+Date:			2024-
+Synopsis:		
+Filename:		
+*/
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+ class WorkUserInput {
+
+	 WorkUserInput(ArrayList<Integer> options) {
+	 }
+
+	 Scanner s = new Scanner(System.in);
+}
