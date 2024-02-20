fe6266d3ed2a739c9074b0ac7a2cd1b3c6d8f08f
John-Philipp Vogt <john-philipp.vogt@mailbox.org>
2024-02-20 18:15:18 +0200

    Added first class draft.

---
+++ Meal.java
@@ -0,0 +1,16 @@
+/*
+Author:			John-Philipp Vogt
+Date:			2024-02-20	
+Synopsis:		Class implementing the meal object.
+Filename:		Meal.java
+*/
+
+class Meal {
+	String name;
+	// To do: datetime.date = date;
+	ArrayList<Ingredients> listOfIngredients;
+
+	Meal()  {
+		System.out.println("This is a meal.");
+	}
+}
